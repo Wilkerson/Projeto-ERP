453d3bac9a142810c408667d236a8a8b3f8d67d7
Wilkerson Carlos <wilkersoncarlos@gmail.com>
2026-03-12 19:08:14 -0300

    model funcionario implementado

--- erp/models.py
+++ erp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Funcionario(models.Model):
+    nome = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False
+    )
+    
+    sobrenome = models.CharField(
+        max_length=70,
+        null=False,
+        blank=False
+    )
+    
+    cpf = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+    
+    email_funcional = models.EmailField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    
+    remuneracao = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
