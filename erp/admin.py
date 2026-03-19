4224dca8c0638d1f592edb82270ea1571aa8ad95
Wilkerson Carlos <wilkersoncarlos@gmail.com>
2026-03-19 10:39:38 -0300

    models registrados no painel administrativo do django

--- erp/admin.py
+++ erp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from erp.models import Funcionario, Produto, Venda
+
 # Register your models here.
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'cpf', 'email_funcional', 'remuneracao')
+    
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'preco')
+    
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('funcionario', 'produto', 'data_hora')
+    
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Venda, VendaAdmin)
+    
